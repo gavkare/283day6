efaab4f7a3fe89e81d99085343261952513dcb24
Shankar Gavkare <102272115+gavkare@users.noreply.github.com>
2023-05-25 23:25:50 +0530

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,18 @@
+
+
+    public class Node<T> {
+        T key;
+        Node<T> next;
+
+        public Node() {
+
+        }
+
+        public Node(T key ) {
+            this.key = key;
+
+        }
+
+
+    }
+
